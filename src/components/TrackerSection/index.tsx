e2fe590e86b64bfcbd0da1fe1b1c7e6596ccf4dc
hemorgenes <kauan7622@gmail.com>
2022-11-08 14:51:28 -0300

    teste

--- src/components/TrackerSection/index.tsx
+++ src/components/TrackerSection/index.tsx
@@ -1,27 +1,11 @@
 import Link from "next/link";
-import React, { useState } from "react";
 import styles from "./styles.module.scss";
-import ReactLoading from "react-loading";
 
 function TrackerSection() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   return (
-    <Link href="/tracker">
+    <Link href="/rastreador">
       <div className={styles.container}>
-        {isLoading ? (
-          <h2>
-            <ReactLoading
-              className={styles.loading}
-              type="spinningBubbles"
-              width={32}
-              height={32}
-              color="#1a1a1a"
-            />
-          </h2>
-        ) : (
-          <h2 onClick={() => setIsLoading(true)}>CONHEÇA O FUTURO</h2>
-        )}
+        <h2>CONHEÇA O FUTURO</h2>
       </div>
     </Link>
   );
---
+++ src/utils/content.ts
@@ -0,0 +1,44 @@
+ type AdvtageProps = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const advantage: AdvtageProps[] = [
+  {
+    id: 1,
+    title: "Tecnologia 4G",
+    description:
+      "Diferentes de outras empresas, a Nettracker utiliza em seus rastreadores a tecnologia 4G. Além de ser a maior tecnologia móvel no Brasil, em questão de velocidade se torna superior ao 3G, o qual está presente nos rastreadores atuais.",
+  },
+  {
+    id: 2,
+    title: "Chip multi-operadoras",
+    description:
+      "O nosso chip é multi-operadoras. Isso significa que caso o seu rastreador sair de área porque não foi possível se conectar a torre, ele automaticamente se conectará com uma torre de operadora diferente.",
+  },
+  {
+    id: 3,
+    title: "Tecnologia Híbrida",
+    description:
+      "Possuímos em nosso rastreador uma tecnologia híbrida. Isso significa que o nosso rastreador é compativel com qualquer tecnologia que esteja atribuida a seu veículo, independente se tal for Autotrac, Ominilink, Onix, entre outras.",
+  },
+  {
+    id: 4,
+    title: "Compatibilidade",
+    description:
+      "O rastreador Nettracker possui o menor equipamento do mercado, fornecendo a maior compatibilidade e discrição. Contudo, não significa que nosso equipamento é frágil, pelo ao contrario. Além de todos os beníficos, o rastreador apresenta uma durabilidade consistente.",
+  },
+  {
+    id: 5,
+    title: "100% Brasileiro",
+    description:
+      "Todos os periféricos são produzidos no Brasil, desde o desenvolvimento até a montagem. A Nettracker traz toda a originalidade e hosnetidade para os clientes.",
+  },
+  {
+    id: 6,
+    title: "Rede 485",
+    description:
+      "Uma de suas principais vantagens é que essa rede permite a instalação de vários dispositivos RS485 no mesmo barramento, o que torna possível a conexão de vários nós entre si. Logo, conseguimos operar várias travas, sensores, sensores de temperatura de forma simultânea e individual.",
+  },
+];
